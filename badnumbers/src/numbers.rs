d0b4c39849326ac649b3755ba8408b2583ab6cd5
Ryan Peden <ryan04@gmail.com>
2014-11-21 20:23:44 -0500

    added bad numbers program that should not be able to compile

---
+++ badnumbers/src/numbers.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let x: int;
+
+    //compilation should fail since x hasn't 
+    //been assigned a value before we try to 
+    //use it
+    println!("The value of x is: {}", x);
+}
