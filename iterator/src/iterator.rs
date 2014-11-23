8a6f7264cee8c64cd39966c9d043f7ae5ef0e2f9
Ryan Peden <ryan04@gmail.com>
2014-11-23 06:05:58 -0500

    modified iterator to test iteration without using range

--- iterator/src/iterator.rs
+++ iterator/src/iterator.rs
@@ -1,5 +1,10 @@
 fn main() {
    for x in range(0i, 10i) {
        println!("{}", x);
-   } 
+   }
+   
+   let nums = vec![11i, 12i, 13i];
+   for num in nums.iter() {
+      println!("{}", num);
+   }      
 }
