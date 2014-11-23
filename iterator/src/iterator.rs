9b0349940ce6655ff6a1a0d54ddfe439529f057d
Ryan Peden <ryan04@gmail.com>
2014-11-23 06:11:50 -0500

    mofidied iterator program to test iterator collector and pattern matching

--- iterator/src/iterator.rs
+++ iterator/src/iterator.rs
@@ -6,5 +6,12 @@ fn main() {
    let nums = vec![11i, 12i, 13i];
    for num in nums.iter() {
       println!("{}", num);
-   }      
+   }
+
+   let greater_than_55 = range(0i, 125i).find(|x| *x > 55);
+   
+   match greater_than_55 {
+     Some(_) => println!("Houston, we have numbers"),
+     None    =>	println!("Nothing found :("),
+   }
 }
