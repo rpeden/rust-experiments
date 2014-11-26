a13913da47cfa50f0d48199ef66ec62892313d29
Ryan Peden <ryan04@gmail.com>
2014-11-26 07:46:11 -0500

    removed enum because Rust has it built in

--- generics/src/generics.rs
+++ generics/src/generics.rs
@@ -1,8 +1,3 @@
-enum Result<T, E> {
-  Ok(T), 
-  Err(E),
-}
-
 fn inverse(x: f64) -> Result<f64, String> {
   if x == 0.0f64 { return Err("x cannot be zero!".to_string()); }
 
@@ -10,5 +5,10 @@ fn inverse(x: f64) -> Result<f64, String> {
 }
 
 fn main() {
-   
+   let x = inverse(25.0f64);
+
+   match x {
+    Ok(x) => println!("The inverse is: {}", x),
+    Err(msg) => println!("Error: {}", msg),
+   }
 }
