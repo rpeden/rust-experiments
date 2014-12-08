6b4db7d047a835c2db28a98718de7041920dc946
Ryan Peden <ryan04@gmail.com>
2014-12-07 21:38:41 -0500

    added code to test circle constructor

--- methods/src/methods.rs
+++ methods/src/methods.rs
@@ -21,4 +21,7 @@ impl Circle {
 fn main() {
   let c = Circle { x: 0.0, y: 0.0, radius: 2.0 };
   println!("Circle's area is {}", c.area());
+
+  let newCircle = Circle::new(1.0, 1.0, 34.0);
+  println!("newCircle's area is {}", newCircle.area());
 }
