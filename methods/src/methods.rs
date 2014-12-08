c4b79c3c12036d629f419dd9c760ba5069fb68d8
Ryan Peden <ryan04@gmail.com>
2014-12-07 21:39:31 -0500

    modified variable name to follow compiler's style suggestions

--- methods/src/methods.rs
+++ methods/src/methods.rs
@@ -22,6 +22,6 @@ fn main() {
   let c = Circle { x: 0.0, y: 0.0, radius: 2.0 };
   println!("Circle's area is {}", c.area());
 
-  let newCircle = Circle::new(1.0, 1.0, 34.0);
-  println!("newCircle's area is {}", newCircle.area());
+  let new_circle = Circle::new(1.0, 1.0, 34.0);
+  println!("newCircle's area is {}", new_circle.area());
 }
